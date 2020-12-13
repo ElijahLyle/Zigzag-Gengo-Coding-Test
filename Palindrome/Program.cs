6f82be3e5687d5e19c0e18ab28385a242ddb45ed
Elijah Lyle Minguez <eminguez19@gmail.com>
2020-12-13 18:20:39 +0800

    Posting coding test solution

---
+++ Palindrome/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        private List<String> list;
+        static void Main(string[] args)
+        {
+            Program run = new Program();
+            String r = "Step on no pets";
+            Console.WriteLine(r);
+            Console.WriteLine(run.checkPalindrome(r));
+            Console.WriteLine(run.longestPalindrome(r));
+            Console.WriteLine(run.cutPalindrome(r));
+        }
+
+        private static string reverseString(String s)
+        {
+            char[] array = Regex.Replace(s, @"\s+", "").ToUpper().ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        public Boolean checkPalindrome(String s)
+        {
+            return Regex.Replace(s, @"\s+", "").ToUpper().Equals(reverseString(s));
+        }
+
+        private int findPalindrome(String original)
+        {
+            list = new List<String>();
+            int index_of_longest = 0;
+            String reversed = reverseString(original);
+            original = Regex.Replace(original, @"\s+", "").ToUpper();
+            String palindrome = original.Substring(0, 1);
+            for (int a = 1; a <= original.Length; a++)
+            {
+                if (a == original.Length)
+                    palindrome += " ";
+                else
+                    palindrome += original[a];
+                if (!reversed.Contains(palindrome))
+                {
+                    list.Add(palindrome.Substring(0, palindrome.Length - 1));
+                    reversed = reversed.Remove(reversed.IndexOf(palindrome.Substring(0, palindrome.Length - 1)),
+                        palindrome.Substring(0, palindrome.Length - 1).Length);
+                    palindrome = palindrome.Substring(palindrome.Length - 1, 1);
+                    if (list.Count > 1)
+                    {
+                        if (list[list.Count - 1].Length > list[index_of_longest].Length)
+                            index_of_longest = list.Count - 1;
+                    }
+                }
+            }
+            return index_of_longest;
+        }
+
+        public String longestPalindrome(String s)
+        {
+            int index = findPalindrome(s);
+            return list[index];
+        }
+
+        public int cutPalindrome(String s)
+        {
+            int index = findPalindrome(s);
+            int number_of_cuts = 0;
+            foreach (String element in list)
+                if (element.Length > 1)
+                    number_of_cuts++;
+            return number_of_cuts;
+        }
+    }
+}
---
+++ PalindromeTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace PalindromeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private string input_1 = "abcdcba";
+        private string input_2 = "abaxyzzyxf";
+        private string input_3 = "noonabbad";
+        private string input_4 = "Step on no pets";
+
+        Program palindrome = new Program();
+
+        [TestMethod]
+        public void Palindrome_Level1()
+        {
+            //Assert.IsTrue(palindrome.checkPalindrome(input_1));
+            Assert.AreEqual(true, palindrome.checkPalindrome(input_1));
+            Assert.AreEqual(false, palindrome.checkPalindrome(input_2));
+            Assert.AreEqual(false, palindrome.checkPalindrome(input_3));
+            Assert.AreEqual(true, palindrome.checkPalindrome(input_4));
+        }
+
+        [TestMethod]
+        public void Palindrome_Level2()
+        {
+            Assert.AreEqual("abcdcba".ToUpper(), palindrome.longestPalindrome(input_1));
+            Assert.AreEqual("xyzzyx".ToUpper(), palindrome.longestPalindrome(input_2));
+            Assert.AreEqual("noon".ToUpper(), palindrome.longestPalindrome(input_3));
+        }
+
+        [TestMethod]
+        public void Palindrome_Level3()
+        {
+            Assert.AreEqual(1, palindrome.cutPalindrome(input_1));
+            Assert.AreEqual(2, palindrome.cutPalindrome(input_2));
+            Assert.AreEqual(2, palindrome.cutPalindrome(input_3));
+            Assert.AreEqual(1, palindrome.cutPalindrome(input_4));
+        }
+    }
+}
